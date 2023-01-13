3ba199218016af36fbd57ed0058a6c739aba26e8
José Esteves <jose.esteves@gmail.com>
2023-01-13 19:01:10 +0000

    C005: Create example data for the achievement

---
+++ src/api/achievement-data-example.js
@@ -0,0 +1,21 @@
+export const achievementExampleData = {
+	id: '56709921-07c6-4f76-be17-38b1d78b58c3',
+	title: 'Climb mount Everest in a year',
+	currentValue: 2344,
+	targetValue: 8144,
+	unit: 'm',
+	targetDay: '2024/01/01',
+	icon: 'mdi-bulb',
+};
+export const timeStampsExampleData = {
+	id: '56709921-07c6-4f76-be17-38b1d78b58c3',
+	timeStamps: [
+		{ date: '2023/01/13', value: 100 },
+		{ date: '2023/01/14', value: 200 },
+		{ date: '2023/01/16', value: 233 },
+		{ date: '2023/01/17', value: 454 },
+		{ date: '2023/01/18', value: 567 },
+		{ date: '2023/01/20', value: 789 },
+		{ date: '2023/01/21', value: 799 },
+	],
+};
